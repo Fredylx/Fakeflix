454d67518465cacb5d96892e9241091f2d3e6a72
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-10-27 21:25:57 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey Clever Fredy! Lets' build this Netflix clone Front-end! 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
